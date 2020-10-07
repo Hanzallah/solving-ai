ee030232ef12540fa036b3e9a6bf031aa2e49047
Hanzallah <hanzallahazimburney@live.com>
2020-10-07 20:05:16 +0300

    Update solution.py

--- missionaries-and-cannibals.py/solution.py
+++ missionaries-and-cannibals.py/solution.py
@@ -2,34 +2,42 @@
 A DFS search solution to the missionaries and cannibals problem
 '''
 
-class StateSearch():
-    def __init__(self, cannibal_left, missionaries_left, boat_direction, boat_size):
+class State():
+    def __init__(self, c_left, m_left, c_right, m_right, boat_direction, boat_size):
         self.parent = None
+        self.c_left = c_left
+        self.m_left = m_left
+        self.c_right = c_right
+        self.m_right = m_right
         self.boat_direction = boat_direction
         self.boat_size = boat_size
-        self.cannibal_left = cannibal_left
-        self.missionaries_left = missionaries_left
-    
-    def goal_state(self):
-        return (self.cannibal_left == 0 and self.missionaries_left == 0)
+        
+    #  function to check if the goal state has been achieved
+    def is_goal_state(self):
+        return (self.c_left == 0 and self.m_left == 0)
 
+    # validate the state under the problem constraints
     def validate(self):
-        pass
+        return (self.m_left == 0 or self.m_left >= self.c_left) and (self.m_right == 0 or self.m_right >= self.c_right) \
+            and (self.m_left >= 0 and self.m_right >= 0) and (self.c_left >= 0 and self.c_right >= 0)
 
+    # check if two states are equal
     def __eq__(self, other_state):
-        return self.cannibal_left == other_state.cannibal_left and self.missionary_left == other_state.missionary_left \
+        return self.c_right == other_state.c_right and self.m_right == other_state.m_right \
+            and self.c_left == other_state.c_left and self.m_left == other_state.m_left \
             and self.boat_direction == other_state.boat_direction and self.boat_size == other_state.boat_size
 
+# TODO
 def create_children(state):
     pass
 
 '''
 A depth-first search algorithm to find the goal state
 '''
-def dfs(cannibal_left, missionaries_left, boat_direction, boat_size):
-    init_state = StateSearch(cannibal_left, missionaries_left, boat_direction, boat_size)
+def dfs(c_left, m_left, c_right, m_right, boat_direction, boat_size):
+    init_state = State(c_left, m_left, c_right, m_right, boat_direction, boat_size)
 
-    if init_state.goal_state():
+    if init_state.is_goal_state():
         return init_state()
 
     # create the visisted set and the stack
@@ -40,7 +48,7 @@ def dfs(cannibal_left, missionaries_left, boat_direction, boat_size):
     while stack:
         # remove and check the top state on the stack
         top_state = stack.pop()
-        if (top_state.goal_state()):
+        if (top_state.is_goal_state()):
             return top_state
         
         # if the top state is not the goal state then add it to the visisted set
@@ -55,12 +63,13 @@ def dfs(cannibal_left, missionaries_left, boat_direction, boat_size):
                 stack.append(child)
     return None
 
-def print_path():
+# TODO
+def print_path(solution):
     pass
 
 def main():
-    solution = dfs(3,3,'left',2)
-    print_path()
+    solution = dfs(5,5,0,0,'left',3)
+    print_path(solution)
 
 if __name__ == "__main__":
     main()
