c52af14ef0114f82baf499d65526023e1089f2c1
Hanzallah <hanzallahazimburney@live.com>
2020-10-07 16:56:58 +0300

    Update solution.py

--- missionaries-and-cannibals.py/solution.py
+++ missionaries-and-cannibals.py/solution.py
@@ -11,13 +11,14 @@ class StateSearch():
         self.missionaries_left = missionaries_left
     
     def goal_state(self):
-        pass
+        return (self.cannibal_left == 0 and self.missionaries_left == 0)
 
     def validate(self):
         pass
 
     def __eq__(self, other_state):
-        pass
+        return self.cannibal_left == other_state.cannibal_left and self.missionary_left == other_state.missionary_left \
+            and self.boat_direction == other_state.boat_direction and self.boat_size == other_state.boat_size
 
 def create_children(state):
     pass
