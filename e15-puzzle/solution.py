1d1e811d8ab3f63c50c84f032ee79865f87c5ca5
YusufZD <dilekyusufziya@gmail.com>
2020-10-22 01:18:58 +0300

    Update solution.py
    Created a Node class that represents the Puzzle.
    Implemented h1 admissable heuristic
    Overwrote the less than, greater than and equals

--- e15-puzzle/solution.py
+++ e15-puzzle/solution.py
@@ -8,6 +8,75 @@ NOTE FROM -> Yusuf Ziya Dilek
 Beware that 10 scrambles does not scramble very much, especially top left
 does not change much since the empty tile starts from the bottom right
 '''
+
+
+class Node:
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.estimatedDistanceToGoal = self.h1Heuristic()
+
+    # Override EQUALS (==)
+    def __eq__(self, other):
+        rows = len(self.puzzle)
+        columns = len(self.puzzle[0])
+        for i in range(0, rows):
+            for j in range(0, columns):
+                if self.puzzle[i][j] != other.puzzle[i][j]:
+                    return False
+        return True
+
+    # Override LESS THAN (<)
+    def __lt__(self, other):
+        if self.estimatedDistanceToGoal < other.estimatedDistanceToGoal:
+            return True
+        else:
+            return False
+
+    # Override GREATER THAN (>)
+    def __gt__(self, other):
+        if self.estimatedDistanceToGoal > other.estimatedDistanceToGoal:
+            return True
+        else:
+            return False
+
+    # GET possible Nodes that we can go from this Node
+    def getAllNextPossibleNodes():
+        print("DO this next")
+
+    # Find the admissable heuristic of this puzzle
+    def h1Heuristic(self):
+        h1 = 0
+        rows = len(self.puzzle)
+        columns = len(self.puzzle[0])
+        finalState = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 5], [4, 5, 5, -1]]
+        for i in range(0, rows):
+            for j in range(0, columns):
+                if (self.puzzle[i][j]-finalState[i][j]) != 0:
+                    h1 = h1 + 1
+        return h1
+
+    #  Display the Puzzle in a neat way
+    def display(self):
+        rows = len(self.puzzle)
+        columns = len(self.puzzle[0])
+        print("-----------------")
+        for i in range(0, rows):
+            print("| ", end="")
+            for j in range(0, columns):
+                if self.puzzle[i][j] == -1:
+                    if j == 0:
+                        print(f"  | ", end="")
+                    else:
+                        print(f"  | ", end="")
+                else:
+                    if j == 0:
+                        print(f"{self.puzzle[i][j]} | ", end="")
+                    else:
+                        print(f"{self.puzzle[i][j]} | ", end="")
+            print()
+        print("-----------------")
+
+
 def createPuzzle():
     # Note -1 is the empty tile
     initState = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 5], [4, 5, 5, -1]]
@@ -73,32 +142,38 @@ def getEmptyTilePosition(puzzle):
     print("There is a problem, no empty tile in this puzzle!!!")
 
 
-def display(puzzle):
-    rows = len(puzzle)
-    columns = len(puzzle[0])
-    print("-----------------")
-    for i in range(0, rows):
-        print("| ", end="")
-        for j in range(0, columns):
-            if puzzle[i][j] == -1:
-                if j == 0:
-                    print(f"  | ", end="")
-                else:
-                    print(f"  | ", end="")
-            else:
-                if j == 0:
-                    print(f"{puzzle[i][j]} | ", end="")
-                else:
-                    print(f"{puzzle[i][j]} | ", end="")
-        print()
-    print("-----------------")
+
 
 
 def main():
+    '''
+    Just Testing the functions and classes
+    '''
+    '''
     for i in range(10):
         newPuzzle = createPuzzle()
         display(newPuzzle)
         print()
+    '''
+    '''
+    nd = Node([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 5], [4, 5, 5, -1]], 3)
+    print(nd.puzzle)
+    nd2 = Node([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 5], [4, 5, 5, -1]], 5)
+    if nd < nd2:
+        print("Less than")
+    else:
+        print("Not less")
+    '''
+    '''
+    puzzle = createPuzzle()
+    print(puzzle)
+    print(h1Heuristic(puzzle))
+    '''
+    nd = Node(createPuzzle())
+    nd.display()
+    print(f"h1 heuristic = {nd.estimatedDistanceToGoal}")
+
+
 
 
 if __name__ == "__main__":
